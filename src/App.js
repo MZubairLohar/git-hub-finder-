b1ad344c313a5a2276e355f90e307d86a35c8b9d
MZubairLohar <zubairgituser@gmail.com>
2020-04-09 16:41:59 +0500

    expression and conditional jsx

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = "Zubair";
+    const loading = false;
+    const showName = true;
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1> }
+      
      
     </div>
   );
