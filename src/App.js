cfaf52e4c9bd47a62f7043cefa0f4985592b724c
MZubairLohar <zubairgituser@gmail.com>
2020-04-09 15:52:52 +0500

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css'; 
 
-function App() {
-
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
+      <h1>Hello from React</h1>
+     
     </div>
   );
+}
 }
 
 export default App;
