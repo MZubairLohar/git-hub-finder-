b32a3e68e20a51ab7d3cf5d07ecaac788f6b06c4
MZubairLohar <zubairgituser@gmail.com>
2020-04-16 17:38:50 +0500

    linked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import Navbar from './componants/layout/Navbar.js'; 
 import Users from './componants/user/Users.js';
+import axios from 'axios';
 import './App.css';
 
+
+
 class App extends Component {
+  state = {
+    users : [],
+    loading : false
+  }
+
+  async componentDidMount(){
+    const res = await axios.get('https://api.github.com./users');
+    console.log(res.data);
+  }
   render() {
   
-    
+
   return (
     <div className="App" >
       <Navbar />
